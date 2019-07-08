cd29c306c058cbb5924016971f1062bdda731eb8
刘东阳 <liudongyang@MacBook-Pro.local>
2019-07-08 14:13:28 +0800

    Initial commit

---
+++ src/Hanoi.java
@@ -0,0 +1,17 @@
+public class Hanoi {
+
+  private static void func(int n, String from, String help, String to) {
+    if (n == 1) {
+      System.out.println("move from " + from + " to " + to);
+    } else {
+      func(n - 1, from, to, help);
+      func(1, from, help, to);
+      func(n - 1, help, from, to);
+      System.out.println("{}}]]{}}{}{{{}{}{}{}{「」「」「」{}");
+    }
+  }
+
+  public static void main(String[] args) {
+    func(3, "left", "mid", "right");
+  }
+}
